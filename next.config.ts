f55659f0181cce0200e5a79f0aedf4648bfad0d2
subhranshudash13-dotcom <subhranshu.dash13@gmail.com>
2026-01-29 01:46:18 +0530

    Fix next.config.ts types

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
+    // @ts-ignore
     ignoreBuildErrors: true,
   },
   eslint: {
+    // @ts-ignore
     ignoreDuringBuilds: true,
   },
 };
